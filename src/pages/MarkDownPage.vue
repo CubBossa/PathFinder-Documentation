20c13a736e7eb984d2711c5741f6f9edfd429b28
CubBossa <leonard.bausenwein@web.de>
2022-10-03 08:56:27 +0200

    static imports

--- src/pages/MarkDownPage.vue
+++ src/pages/MarkDownPage.vue
@@ -69,7 +69,7 @@ function loadMarkdown() {
   }
 
   if (projects.currentPage?.markdown !== undefined) {
-    import('src/assets/markdown/' + projects.currentProject?.key + '/' + projects.currentVersion?.version + '/' + projects.currentPage?.markdown + '.md').then(value => {
+    import('../assets/markdown/' + projects.currentProject?.key + '/' + projects.currentVersion?.version + '/' + projects.currentPage?.markdown + '.md').then(value => {
       markdown.value = value.default
     })
   }
